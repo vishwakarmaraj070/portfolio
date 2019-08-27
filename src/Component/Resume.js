ecfea896ca4eda86ebd81a21b968c45fe75f9b1b
Raj <vishwakarmaraj070@gmail.com>
2019-08-27 15:17:58 +0530

    talk chnages

--- src/Component/Resume.js
+++ src/Component/Resume.js
@@ -341,17 +341,13 @@ export default class Resume extends Component {
                         <li className="mb-1">UI / UX Design</li>
                         <li className="mb-1">Listening Songs</li>
                         <li className="mb-1">Playing Cricket</li>
-                        <li className="mb-1">Success Developer</li>
+                        <li className="mb-1">Frontend Adda</li>
                       </ul>
                     </div>
                   </section>
                   <section className="resume-section interests-section">
-                    <h2 className="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">See More</h2> 
-                    <div className="resume-section-content">
-                     <p>Interest to see my all project go to my <a href="https://rajv-portfolio.herokuapp.com/" target="_blank">Portfolio website</a></p>
-                    </div>
+                    <a className="btn btn-primary btn-sm" href={require('../assets/resume.pdf')} download="Raj-Resume" target="_blank"><svg className="svg-inline--fa fa-download fa-w-16 mr-2" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="download" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M216 0h80c13.3 0 24 10.7 24 24v168h87.7c17.8 0 26.7 21.5 14.1 34.1L269.7 378.3c-7.5 7.5-19.8 7.5-27.3 0L90.1 226.1c-12.6-12.6-3.7-34.1 14.1-34.1H192V24c0-13.3 10.7-24 24-24zm296 376v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h146.7l49 49c20.1 20.1 52.5 20.1 72.6 0l49-49H488c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"></path></svg>Download PDF Version</a>
                   </section>
-                  <a className="btn btn-primary btn-sm" href={require('../assets/resume.pdf')} download="Raj-Resume" target="_blank"><svg className="svg-inline--fa fa-download fa-w-16 mr-2" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="download" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M216 0h80c13.3 0 24 10.7 24 24v168h87.7c17.8 0 26.7 21.5 14.1 34.1L269.7 378.3c-7.5 7.5-19.8 7.5-27.3 0L90.1 226.1c-12.6-12.6-3.7-34.1 14.1-34.1H192V24c0-13.3 10.7-24 24-24zm296 376v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h146.7l49 49c20.1 20.1 52.5 20.1 72.6 0l49-49H488c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"></path></svg>Download PDF Version</a>
                 </div>
               </div>
             </div>
